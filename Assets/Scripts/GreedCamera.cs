f77bd5d0293bbabe3f3929836a9a16faa48bfb13
Grosdemouge, Cristol <cristol.gdm@gmail.com>
2020-06-10 17:40:10 +0900

    Functioning sprite in canvas (baby steps)

---
+++ Assets/Scripts/GreedCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class GreedCamera : MonoBehaviour {
+    private Canvas canvas;
+    // Start is called before the first frame update
+    void Start() {
+        canvas = gameObject.GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (canvas == null) return;
+        if (canvas.worldCamera != null) return;
+
+        Camera camera = Camera.main;
+
+        Debug.Log(camera);
+
+        canvas.worldCamera = camera;
+    }
+}
