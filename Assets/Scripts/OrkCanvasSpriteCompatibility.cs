17f9ed7ef5afab15c470dfb02c96614166e086ce
Grosdemouge, Cristol <cristol.gdm@gmail.com>
2020-06-10 17:41:10 +0900

    Pass sprite as background to ORK canvas

---
+++ Assets/Scripts/OrkCanvasSpriteCompatibility.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class OrkCanvasSpriteCompatibility : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Canvas canvas;
+        Canvas[] parentCanvases = gameObject.GetComponentsInParent<Canvas>();
+        if (parentCanvases == null) return;
+        if (parentCanvases.Length == 0) return;
+        canvas = parentCanvases[0];
+        if (canvas == null) return;
+        if (canvas.renderMode == RenderMode.ScreenSpaceCamera) return;
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        if (canvas.worldCamera != null) return;
+
+        Camera camera = Camera.main;
+
+        canvas.worldCamera = camera;
+    }
+}
