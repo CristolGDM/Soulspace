8a7ce895aee003786c23eb9b2ccfc6b31e56cec3
Grosdemouge, Cristol <cristol.gdm@gmail.com>
2020-06-18 22:20:37 +0900

    Spawn points can turn player

---
+++ Assets/Scripts/SpawnPlayerDirection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ORKFramework;
+
+public class SpawnPlayerDirection : MonoBehaviour {
+
+    private enum Direction { left, right, up, down};
+    [SerializeField]
+    private Direction direction = Direction.down;
+
+    // Start is called before the first frame update
+    void Start() {
+        GameObject player = ORK.Game.ActiveGroup.Leader.GameObject;
+        if(Vector2.Distance(player.transform.position, gameObject.transform.position) >= 1) return;
+        Animator animator = player.GetComponent<Animator>();
+        if (animator == null) return;
+
+        switch (direction) {
+            case Direction.down:
+                animator.SetInteger("Direction", 0);
+                break;
+            case Direction.left:
+                animator.SetInteger("Direction", 1);
+                break;
+            case Direction.up:
+                animator.SetInteger("Direction", 2);
+                break;
+            case Direction.right:
+                animator.SetInteger("Direction", 3);
+                break;
+            default:
+                break;
+        }
+    }
+}
